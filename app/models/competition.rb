cd15124d8dc03769a91fd5688761085d9500f410
Matt Toensing <matthew.toensing@gmail.com>
2021-08-04 08:54:33 -0700

    Add model test and method for average player age

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,8 @@
 class Competition < ApplicationRecord
   has_many :competition_teams
   has_many :teams, through: :competition_teams
+
+  def average_player_age
+    self.teams.joins(:players).average(:age).to_i
+  end
 end
--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe Competition do
     it { should have_many(:competition_teams) }
     it { should have_many(:teams).through(:competition_teams) }
   end
+
+  describe 'instance methods' do
+    describe '#average_player_age' do
+      it 'returns the average age of all players associated with teams in the competition' do
+        competition = create(:competition)
+        team1 = create(:team)
+        player1 = create(:player, age: 33, team: team1)
+        player2 = create(:player, age: 37, team: team1)
+        team2 = create(:team)
+        player3 = create(:player, age: 28, team: team2)
+        player4 = create(:player, age: 32, team: team2)
+        comp_team1 = CompetitionTeam.create(team: team1, competition: competition)
+        comp_team2 = CompetitionTeam.create(team: team2, competition: competition)
+
+        expect(competition.average_player_age).to eq(32)
+      end
+    end
+  end
 end

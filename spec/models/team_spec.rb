52e5e3add4db44ed1da767df45169367b7762ba6
Matt Toensing <matthew.toensing@gmail.com>
2021-08-04 09:55:16 -0700

    Include model test for teams class method

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe Team do
     it { should have_many(:competition_teams) }
     it { should have_many(:competitions).through(:competition_teams) }
   end
+
+  describe 'class methods' do
+    describe '::average_age_of_players' do
+      it 'returns teams sorted by average player age in asending order and also a column for average player age' do
+        team1 = create(:team)
+        player1 = create(:player, age: 33, team: team1)
+        player2 = create(:player, age: 35, team: team1)
+        team2 = create(:team)
+        player1 = create(:player, age: 28, team: team2)
+        player2 = create(:player, age: 32, team: team2)
+
+        expected = [team1, team2]
+        result = Team.average_age_of_players
+
+        expect(result[0].avg_age).to eq(34)
+        expect(result[1].avg_age).to eq(30)
+      end
+    end
+  end
 end

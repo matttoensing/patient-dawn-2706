dc2929b9a3ec15b3ee1eee6f186cfc9c99838994
Matt Toensing <matthew.toensing@gmail.com>
2021-08-04 08:55:50 -0700

    Include test and content for average player age of competition

--- spec/features/competitions/show_spec.rb
+++ spec/features/competitions/show_spec.rb
@@ -12,12 +12,42 @@ RSpec.describe 'competition show page' do
       expect(page).to have_content("Sport: #{competition.sport}")
     end
 
-    it '' do
-      # As a user
-      # When I visit a competition's show page
-      # Then I see the competition's name, location, and sport
-      # And I see the nickname and hometown of all teams in this competition
-      # And I see the average age of all players in the competition
+    it 'page displays the nickname and hometown of all teams in the competition' do
+      competition = create(:competition)
+      team1 = create(:team)
+      team2 = create(:team)
+      comp_team1 = CompetitionTeam.create(team: team1, competition: competition)
+      comp_team2 = CompetitionTeam.create(team: team2, competition: competition)
+
+      visit "/competitions/#{competition.id}"
+
+      within "#team-#{team1.id}" do
+        expect(page).to have_content("Nickname: #{team1.nickname}")
+        expect(page).to have_content("Hometown: #{team1.hometown}")
+      end
+
+      within "#team-#{team2.id}" do
+        expect(page).to have_content("Nickname: #{team2.nickname}")
+        expect(page).to have_content("Hometown: #{team2.hometown}")
+      end
+    end
+  end
+
+  describe 'average player age statistic' do
+    it 'displays the average age of all player in the competition on the page' do
+      competition = create(:competition)
+      team1 = create(:team)
+      player1 = create(:player, team: team1)
+      player2 = create(:player, team: team1)
+      team2 = create(:team)
+      player3 = create(:player, team: team2)
+      player4 = create(:player, team: team2)
+      comp_team1 = CompetitionTeam.create(team: team1, competition: competition)
+      comp_team2 = CompetitionTeam.create(team: team2, competition: competition)
+
+      visit "/competitions/#{competition.id}"
+
+      expect(page).to have_content(competition.average_player_age)
     end
   end
 end

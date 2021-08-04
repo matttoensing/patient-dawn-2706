e76d5a73a184a96f2de294559c0d4be27d766bc4
Matt Toensing <matthew.toensing@gmail.com>
2021-08-04 09:54:35 -0700

    Add model class method for sorting teams by average age of players

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,8 @@ class Team < ApplicationRecord
   has_many :players
   has_many :competition_teams
   has_many :competitions, through: :competition_teams
+
+  def self.average_age_of_players
+    Team.joins(:players).select('teams.*, ROUND(AVG(players.age)) AS avg_age').group('teams.id').order(Arel.sql('AVG(players.age) DESC'))
+  end
 end
